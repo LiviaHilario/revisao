fd17cdc32735af32575ace642b74459c8fc1213a
Livia Hilario <liviasantoshilario@gmail.com>
2019-09-24 19:08:33 -0300

    conteudo de revisao

--- Revisão/ViewController.swift
+++ Revisão/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+
+    @IBOutlet weak var digiteNomeText: UITextField!
+    
+    @IBAction func cliqueAquiBotao(_ sender: Any) {
+        
+        print("O botao foi clicado")
+        
+        label.text = digiteNomeText.text 
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Rodando")
+        
     }
-
 
 }
 
